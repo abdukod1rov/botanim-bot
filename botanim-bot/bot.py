1be208451703e0eb701871f9228b947125abde20
samandar7 <abdukodirovsamandar8@gmail.com>
2023-07-11 11:53:42 +0500

    Installed poetry

--- botanim-bot/bot.py
+++ botanim-bot/bot.py
@@ -3,7 +3,7 @@ import os
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-
+import message_texts
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,13 +26,23 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not effective_chat:
         logger.warning("effective_chat is None")
         return
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message_texts.GREETINGS)
+
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("effective_chat is None")
+        return
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message_texts.HELP)
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
     start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help)
     application.add_handler(start_handler)
+    application.add_handler(help_handler)
 
     application.run_polling()
---
+++ botanim-bot/message_texts.py
@@ -0,0 +1,25 @@
+GREETINGS = """
+Привет!
+Это телеграм бот книжнога клуба Ботаним
+You can see list of books and plan reading it.
+Join our club https://botanim.to.digital
+
+Bot commands:
+
+/start - Приветсвенное сообшение
+/allboooks - все книгиб который  в нашем списке
+/already - прочитанные книги
+/now - книгаб которую сейчас читаем
+/vote- проголосовать за следющую книгу
+
+
+"""
+
+HELP = """
+
+Our book club operates on a subscribe on our closed telegram group, which cost 1500 RUBL per month
+To join, go to https://t.me/+IyGKU9EIGP5jMTky
+
+If you have not recieved a response, or have any questions email us
+
+"""
